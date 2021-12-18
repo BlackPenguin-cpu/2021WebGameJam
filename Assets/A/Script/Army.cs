00231efc95c9b74d130b4ee6e34626705014e8fa
AUryeong <jykee7030@gmail.coma>
2021-12-18 23:18:09 +0900

    waha

--- Assets/A/Script/Army.cs
+++ Assets/A/Script/Army.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class Army : MonoBehaviour
 {
+    public Node owner;
+    public int create;
+    public Node attack;
+    public RectTransform rect
+    {
+        get
+        {
+            return this.GetComponent<RectTransform>();
+        }
+    }
     void Start()
     {
-        
+        rect.localPosition = owner.GetComponent<RectTransform>().localPosition;
     }
 
     void Update()
