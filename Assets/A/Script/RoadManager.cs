1f5b0071d9932d23fd96527834b3fe4297e5fece
Black_Penguin <dkstmdwnswns@gmail.com>
2021-12-19 05:50:53 +0900

    road Manager

--- Assets/A/Script/RoadManager.cs
+++ Assets/A/Script/RoadManager.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
-public class RoadManager : MonoBehaviour
+public class RoadManager : Singleton<RoadManager>
 {
-    public static RoadManager Instance { get; private set; }
     public List<Node> nodes = new List<Node>();
     public List<Image> nachim = new List<Image>();
     public Node selectnode = new Node();
     public GameObject obj;
     public List<Sprite> sprites = new List<Sprite>();
 
-
+    protected override void Awake()
+    {
+        
+    }
 }
