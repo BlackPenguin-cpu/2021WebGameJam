b57bd4a814c5aee368daddee3f07c2be8b86a843
AUryeong <jykee7030@gmail.coma>
2021-12-18 23:29:38 +0900

    tmp 넣음

--- Assets/A/Script/Army.cs
+++ Assets/A/Script/Army.cs
@@ -21,6 +21,11 @@ public class Army : MonoBehaviour
 
     void Update()
     {
-        
+        Vector2 me = gameObject.GetComponent<RectTransform>().localPosition;
+        Vector2 target = attack.GetComponent<RectTransform>().localPosition;
+        float angle = Mathf.Atan2(me.y - target.y, me.x - target.x) * Mathf.Rad2Deg;
+        rect.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        rect.Translate(Vector3.down * 1);
+        if(rect.localPosition.x - attack.GetComponent<RectTransform>().localPosition.x < 1 && rect.localPosition.y - attack.GetComponent<RectTransform>().localPosition.y < 1)
     }
 }
