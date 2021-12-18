e7bec66cb5658b916f1d6ae23f587225bc232225
AUryeong <jykee7030@gmail.coma>
2021-12-18 23:10:21 +0900

    여러가지 바꿈

--- Assets/A/Script/Arrow.cs
+++ Assets/A/Script/Arrow.cs
@@ -7,6 +7,7 @@ public class Arrow : Interaction
     public Node arrow;
     protected override void Action()
     {
+        Debug.Log(arrow.name);
     }
     void Update()
     {
--- Assets/A/Script/Node.cs
+++ Assets/A/Script/Node.cs
@@ -1,51 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 public class Node : Interaction
 {
     public List<Node> linkednodes;
     public int create;
     public bool mine;
+    public bool your;
     private float duration;
     [SerializeField]
     private Image image;
+    [SerializeField]
+    private TextMeshProUGUI text;
 
     void Awake()
     {
         RoadManager.Instance.nodes.Add(gameObject.GetComponent<Node>());
+        if(!mine && !your)
+        {
+            text = GameObject.Instantiate<TextMeshProUGUI>(text);
+            text.GetComponent<RectTransform>().SetParent(this.GetComponent<RectTransform>());
+            text.GetComponent<RectTransform>().localPosition = Vector3.zero;
+        }
     }
 
     protected override void Action()
     {
-        if(RoadManager.Instance.nachim.Count > 0)
+        if (mine)
         {
-            foreach (Image img in RoadManager.Instance.nachim)
+            if (RoadManager.Instance.nachim.Count > 0)
             {
-                GameObject.Destroy(img.gameObject);
+                foreach (Image img in RoadManager.Instance.nachim)
+                {
+                    GameObject.Destroy(img.gameObject);
+                }
+                RoadManager.Instance.nachim = new List<Image>();
             }
-            RoadManager.Instance.nachim = new List<Image>();
-        }
-        foreach(Node node in linkednodes)
-        {
-            Vector2 me = gameObject.GetComponent<RectTransform>().localPosition;
-            Vector2 target = node.gameObject.GetComponent<RectTransform>().localPosition;
-            float angle = Mathf.Atan2(me.y - target.y, me.x - target.x) * Mathf.Rad2Deg;
-            Image img = GameObject.Instantiate<Image>(image);
-            img.GetComponent<RectTransform>().SetParent(gameObject.GetComponent<RectTransform>());
-            img.GetComponent<RectTransform>().localPosition = Vector3.zero;
-            img.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
-            img.GetComponent<RectTransform>().sizeDelta = new Vector2(1, 1);
-            img.GetComponent<RectTransform>().rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
-            img.GetComponent<RectTransform>().Translate(Vector3.down * 1);
-            img.GetComponent<Arrow>().arrow = node;
-            RoadManager.Instance.nachim.Add(img);
+            foreach (Node node in linkednodes)
+            {
+                Vector2 me = gameObject.GetComponent<RectTransform>().localPosition;
+                Vector2 target = node.gameObject.GetComponent<RectTransform>().localPosition;
+                float angle = Mathf.Atan2(me.y - target.y, me.x - target.x) * Mathf.Rad2Deg;
+                Image img = GameObject.Instantiate<Image>(image);
+                img.GetComponent<RectTransform>().SetParent(gameObject.GetComponent<RectTransform>());
+                img.GetComponent<RectTransform>().localPosition = Vector3.zero;
+                img.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+                img.GetComponent<RectTransform>().sizeDelta = new Vector2(1, 1);
+                img.GetComponent<RectTransform>().rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+                img.GetComponent<RectTransform>().Translate(Vector3.down * 1);
+                img.GetComponent<Arrow>().arrow = node;
+                RoadManager.Instance.nachim.Add(img);
+            }
         }
     }
 
     void Update()
     {
-        if (mine)
+        if (mine || your)
         {
             duration += Time.deltaTime;
             if (duration >= 1)
@@ -54,5 +67,18 @@ public class Node : Interaction
                 duration = 0;
             }
         }
+        if (mine)
+        {
+            text.color = new Color(0, 0, 1);
+        }
+        else if (your)
+        {
+            text.color = new Color(1, 0, 0);
+        }
+        else
+        {
+            text.color = new Color(0, 0, 0);
+        }
+        text.text = create.ToString();
     }
 }
